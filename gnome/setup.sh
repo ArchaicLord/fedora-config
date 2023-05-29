e121d4b46821be50ad1f0fb4c8b708dda49bd16f
ArchaicLord <archaiclord@gmail.com>
2023-05-29 09:26:54 +0100

    prompt for working directioy and wallpapers

--- gnome/setup.sh
+++ gnome/setup.sh
@@ -1,4 +1,46 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+echo "Welcome to Archaic Lord's Fedora Gnome setup"
+
+
+# Function PROMPT used to ask user for input
+PROMPT () {
+  read -p "Would you like to $1? (Y/y. Use any key to ignore): " -n 1 -r $2
+}
+
+
+
+CURRENT_DIR=$(pwd)
+echo "The current working directory is: ${CURRENT_DIR}"
+
+PROMPT "change the wokring directoy"
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo -e "\n"
+    read -p "Enter new working directory location:"$'\n' WORKING_DIR
+    echo "WORKING_DIR set to: ${WORKING_DIR}"
+else
+    WORKING_DIR=$CURRENT_DIR
+    echo -e "\n WORKING_DIR has not been changed from: ${WORKING_DIR}"
+fi
+
+PROMPT "import Archaic's wallpapers?"
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    if [ -d $WORKING_DIR/wallpapers ]
+    then
+	echo -e "\n Archaic's wallpapers are already on this system"
+    else
+	echo "Getting Archaic's wallpaper repo"
+	git clone git@github.com:ArchaicLord/wallpapers.git
+
+	echo "Running wallpaper setup scrip"
+	$WORKING_DIR/wallpapers/setup.sh
+    fi
+fi
+
+
 
 echo "Enabling fractional scaling"
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
